c1f1c82d218337dc2dcf7d27cf2fe171e868bfd2
shinjiyun <ranji12@naver.com>
2024-06-13 12:10:02 +0900

    fill()메서 배열의 인덱 범위 내에 있는 모든 요소를 정적 값으로 변경

--- array/fill.js
+++ array/fill.js
@@ -1,1 +1,9 @@
-console.log('Array.prototype.fill()')
+const arr = [1,2,3,4,5]
+console.log(arr.fill(0, 2, 4)); //[1, 2, 0, 0, 5]
+
+console.log(arr.fill(4, 1)); //[1, 4, 4, 4, 4]
+
+console.log(arr.fill(3)); //[3, 3, 3, 3, 3]
+
+console.log(arr.fill('오', 1, 3)); //[ 3, '오', '오', 3, 3 ]
+
