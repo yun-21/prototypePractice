ffc4ea6a8f8c20f629bd51f4ff1262c6703fcfe1
shinjiyun <ranji12@naver.com>
2024-06-13 17:53:47 +0900

    filter에 대한 부가설명 추가

--- array/filter.js
+++ array/filter.js
@@ -1,5 +1,6 @@
 //* 주어진 배열의 일부에 대한 얕은 복사본을 생성하고, 주어진 배열에서 제공된 함수에 의해 구현된 테스트를 통과한 요소로만 필터링
 
+
 const words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
 
 const result = words.filter((word) => word.length > 6);
@@ -8,6 +9,7 @@ console.log(result);
 // Expected output: Array ["exuberant", "destruction", "pregsent"]
 
 //* 배열을 함수에 통과시켜 새로운 배열 반환한다.
+//* if문이 알아서 들어가있음 and {}가 없으면 암시적 반환이다.
 
 //!
 
