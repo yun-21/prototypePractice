d6a2a3da7f7c904f9d315a91a6545126ef43580f
shinjiyun <ranji12@naver.com>
2024-06-13 12:52:30 +0900

    findIndex()메서드 판별 함수를 만족하는 배열의 첫 번째 요소에 대한 인덱스를 반환합니다.

---
+++ array/findIndex.js
@@ -0,0 +1,22 @@
+//* findIndex() 메서드는 주어진 판별 함수를 만족하는 배열의 첫 번째 요소에 대한 인덱스를 반환합니다. 만족하는 요소가 없으면 -1을 반환합니다.
+const array1 = [5, 12, 8, 130, 44];
+
+const isLargeNumber = (element) => element > 13;
+
+console.log(array1.findIndex(isLargeNumber));
+// Expected output: 3
+
+//*찾고싶은 배열의 요소의 인덱스를 알고 싶을때 사용
+
+//!
+
+//* findIndex()를 배열이 아닌 객체에 호출하기
+const arrayLike = {
+  length: 3,
+  0: 2,
+  1: 7.3,
+  2: 4,
+};
+console.log(
+  Array.prototype.findIndex.call(arrayLike, (x) => !Number.isInteger(x)),
+); // 1
