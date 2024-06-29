24f9348e0bde8eaeea066a00dae79d5b0b1f251e
jiyun <ranji12@naver.com>
2024-06-29 22:00:49 +0900

    if문 배열길이  변경 array배열에 6개의 숫자가 들어간다. (중복된 숫자 발견)

--- lotto/lotto.js
+++ lotto/lotto.js
@@ -1,6 +1,6 @@
 const lotte = () => {
     const array = [];
-    while (array.length < 7) {
+    while (array.length < 6) {
         const random = Math.floor((Math.random() * 45) + 1);
         array.push(random)
     }
