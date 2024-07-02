e04b8dadfa977b72994af30f8dc95705365265f3
jiyun <ranji12@naver.com>
2024-07-02 22:30:09 +0900

    forEach문은 체이닝 불가

--- array/forEach.js
+++ array/forEach.js
@@ -4,3 +4,15 @@ const array = ['a', 'b', 'c'];
 array.forEach((element) => {
     console.log(element)
 });
+
+//* return이 필요 없다. === 반환값을 제공하지 않기 때문입니다
+//* 오름차순 인덱스 순서로 한 번씩 호출한다
+
+//* map()과 달리 forEach()는 항상 undefined를 반환하므로 체이닝할 수 없다.
+const arr = [1, 2, 3];
+
+const result = arr.forEach((num) => {
+  console.log(num);
+});
+
+console.log(result); // undefined
